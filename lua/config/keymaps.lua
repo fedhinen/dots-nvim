3d1b5d5d5ccafc6aa88254cedf52e28f6bc4b8ce
fedhinen <victor.adrian.ruiz.espindola@gmail.com>
2026-02-18 14:56:32 -0600

    temporal config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 local map = vim.keymap.set
-
+vim.diagnostic.config({
+  virtual_text = false,
+  virtual_lines = {
+    current_line = true,
+  },
+})
+vim.wo.number = true
 map("n", "<leader>e", "<cmd>Fyler<cr>", { desc = "Open fyler View" })
